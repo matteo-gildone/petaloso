c42ed2222200b5737b93f7096b211c40281afa8e
Matteo Gildone <matteo.gildone@springernature.com>
2022-07-23 16:24:30 +0100

    setup storybook site

---
+++ apps/storybook-e2e/ui-e2e/src/integration/app.spec.js
@@ -0,0 +1,7 @@
+import { getGreeting } from '../support/app.po';
+describe('site', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=header--primary'));
+  it('should display welcome message', () => {
+    getGreeting().contains('Welcome From Petaloso!');
+  });
+});
